1673b6ce299972e3b627d6826f0d914a737ffc5b
Brage <bragewiseth@icloud.com>
2024-10-19 14:42:15 +0200

    removed docs

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/wone/devel/Cubitron/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/wone/devel/Cubitron/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/wone/devel/Cubitron/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/wone/devel/Cubitron/docs/src/css/custom.css"),
-];
