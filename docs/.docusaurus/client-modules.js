af44fda756c1a852536b48f263e619c6b3f9f228
Felix Strand <zirjamaljamal@gmail.com>
2024-10-19 14:40:00 +0200

    MCU added, x-ed some motordriver pins

---
+++ docs/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/wone/devel/Cubitron/docs/node_modules/infima/dist/css/default/default.css"),
+  require("/home/wone/devel/Cubitron/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/wone/devel/Cubitron/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/wone/devel/Cubitron/docs/src/css/custom.css"),
+];
---
+++ docs/build/assets/js/3a7424fd.f2d2e718.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[402],{2780:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/Cubitron/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
